7ce3852eb81489066efac5d2c1ad2f694c48abfe
standlucian <standlucian@gmail.com>
2017-05-17 16:01:49 +0300

    Modified detectorconstruction to the setup used for the LaBr detector

--- src/DetectorConstruction.cc
+++ src/DetectorConstruction.cc
@@ -83,7 +83,7 @@ G4VPhysicalVolume* DetectorConstruction::Construct()
   // World
   //
   G4double world_sizeXY = 33.5*cm;
-  G4double world_sizeZ  = 55*cm;
+  G4double world_sizeZ  = 110*cm;
   G4Material* world_mat = nist->FindOrBuildMaterial("G4_AIR");
   
   G4Box* solidWorld =    
@@ -120,20 +120,10 @@ G4VPhysicalVolume* DetectorConstruction::Construct()
   elements.clear();
   natoms.clear();
 
-  elements.push_back("C");     natoms.push_back(12);
-  elements.push_back("H");     natoms.push_back(5);
-  elements.push_back("Cl");     natoms.push_back(5);
+  elements.push_back("La");     natoms.push_back(1);
+  elements.push_back("Br");     natoms.push_back(3);
 
-  G4Material* pcb_mat = nist->ConstructNewMaterial("pcb", elements, natoms, 1.374*g/cm3);
-
-  elements.clear();
-  natoms.clear();
-
-  elements.push_back("Cd");     natoms.push_back(1);
-  elements.push_back("Zn");     natoms.push_back(1);
-  elements.push_back("Te");     natoms.push_back(1);
-
-  G4Material* czt_mat = nist->ConstructNewMaterial("czt", elements, natoms, 5.56*g/cm3);
+  G4Material* LaBr_mat = nist->ConstructNewMaterial("LaBr", elements, natoms, 5.06*g/cm3);
 
   elements.clear();
   natoms.clear();
@@ -143,7 +133,7 @@ G4VPhysicalVolume* DetectorConstruction::Construct()
 
   G4Material* uo_mat = nist->ConstructNewMaterial("uo", elements, natoms, 3.29*g/cm3);
 
-  G4Material* pvc_mat     = nist->FindOrBuildMaterial("G4_POLYVINYL_CHLORIDE");
+  G4Material* glass_mat = nist->FindOrBuildMaterial("G4_GLASS_PLATE");
 
   G4Material* al_mat     = nist->FindOrBuildMaterial("G4_Al");
 
@@ -161,7 +151,7 @@ G4VPhysicalVolume* DetectorConstruction::Construct()
 
  G4LogicalVolume* logicShield  = new G4LogicalVolume (Shield_subst2, pb_mat, "logicShield");
 
- new G4PVPlacement(0, G4ThreeVector(0.,0.,-22.5*mm), logicShield, "logicShield", logicWorld, false, 0, checkOverlaps);
+ new G4PVPlacement(0, G4ThreeVector(0.,0.,252.5*mm), logicShield, "logicShield", logicWorld, false, 0, checkOverlaps);
 
   // Sample holder
 
@@ -169,96 +159,64 @@ G4VPhysicalVolume* DetectorConstruction::Construct()
 
  G4LogicalVolume* logicHolder  = new G4LogicalVolume (SampleHolder, PMMA_mat, "logicHolder");
 
- new G4PVPlacement(0, G4ThreeVector(0.,0.,-5.5*mm), logicHolder, "logicHolder", logicWorld, false, 0, checkOverlaps);
+ new G4PVPlacement(0, G4ThreeVector(0.,0.,181.5*mm), logicHolder, "logicHolder", logicWorld, false, 0, checkOverlaps);
 
  // Sample spacer
 
- G4VSolid* SampleSpacer1 = new G4Tubs ( "SampleSpacer1", 41.6/2.*mm, 58.2/2.*mm, 40/2.*mm, 0.*deg, 360.*deg );
+ G4VSolid* SampleSpacer1 = new G4Tubs ( "SampleSpacer1", 0.*mm, 85/2.*mm, 115/2.*mm, 0.*deg, 360.*deg );
 
- G4VSolid* SampleSpacer2 = new G4Tubs ( "SampleSpacer2", 0*mm, 49.9/2.*mm, 28.15/2.*mm, 0.*deg, 360.*deg );
+ G4VSolid* SampleSpacer2 = new G4Tubs ( "SampleSpacer2", 0.*mm, 75/2.*mm, 110/2.*mm, 0.*deg, 360.*deg );
 
- G4VSolid* Spacer_subst = new G4SubtractionSolid ("Spacer_subst", SampleSpacer1, SampleSpacer2, 0, G4ThreeVector(0.,0.,5.925*mm));
+ G4VSolid* Spacer_subst = new G4SubtractionSolid ("Spacer_subst", SampleSpacer1, SampleSpacer2, 0, G4ThreeVector(0.,0.,2.5*mm));
 
  G4LogicalVolume* logicSpacer  = new G4LogicalVolume (Spacer_subst, PMMA_mat, "logicSpacer");
 
- new G4PVPlacement(0, G4ThreeVector(0.,0.,-28*mm), logicSpacer, "logicSpacer", logicWorld, false, 0, checkOverlaps);
+ new G4PVPlacement(0, G4ThreeVector(0.,0.,121.5*mm), logicSpacer, "logicSpacer", logicWorld, false, 0, checkOverlaps);
 
  // Detector Holder
 
- G4VSolid* DetectorHolder1 = new G4Tubs ( "DetectorHolder1", 24/2.*mm, 62/2.*mm, 147/2.*mm, 0.*deg, 360.*deg );
+ G4VSolid* DetectorBackShieldCu = new G4Tubs ( "DetectorBackShieldCu", 85/2.*mm, 91/2.*mm, 136/2.*mm, 0.*deg, 360.*deg );
 
- G4LogicalVolume* logicDetHolder  = new G4LogicalVolume (DetectorHolder1, pvc_mat, "logicDetHolder");
+ G4LogicalVolume* logicDetShieldCu  = new G4LogicalVolume (DetectorBackShieldCu, al_mat, "logicDetShieldCu");
 
- new G4PVPlacement(0, G4ThreeVector(0.,0.,-201.5*mm), logicDetHolder, "logicDetHolder", logicWorld, false, 0, checkOverlaps);
+ new G4PVPlacement(0, G4ThreeVector(0.,0.,-68*mm), logicDetShieldCu, "logicDetShieldCu", logicWorld, false, 0, checkOverlaps);
 
- G4VSolid* DetectorHolder2 = new G4Tubs ( "DetectorHolder2", 24/2.*mm, 41.4/2.*mm, 29.4/2.*mm, 0.*deg, 360.*deg );
+ G4VSolid* DetectorBackShieldPb = new G4Tubs ( "DetectorBackShieldPb", 91/2.*mm, 111/2.*mm, 136/2.*mm, 0.*deg, 360.*deg );
 
- G4LogicalVolume* logicDetHolder2  = new G4LogicalVolume (DetectorHolder2, pvc_mat, "logicDetHolder2");
+ G4LogicalVolume* logicDetShieldPb  = new G4LogicalVolume (DetectorBackShieldPb, pb_mat, "logicDetShieldPb");
 
-  new G4PVPlacement(0, G4ThreeVector(0.,0.,-62.7*mm), logicDetHolder2, "logicDetHolder2", logicWorld, false, 0, checkOverlaps);
-
- G4VSolid* DetectorHolder3 = new G4Cons ( "DetectorHolder3", 0*mm, 62/2.*mm, 0*mm, 45.5/2.*mm, 50.6/2.*mm,0.*deg, 360.*deg);
-
- G4VSolid* DetectorHolder4 = new G4Tubs ( "DetectorHolder4", 0*mm, 24/2.*mm, 51.5/2.*mm, 0.*deg, 360.*deg );
-
- G4VSolid* DetHolder_subst = new G4SubtractionSolid ("DetHolder_subst", DetectorHolder3, DetectorHolder4, 0, G4ThreeVector(0.,0.,0.*mm));
-
- G4LogicalVolume* logicDetHolder3  = new G4LogicalVolume (DetHolder_subst, pvc_mat, "logicDetHolder3");
-
- new G4PVPlacement(0, G4ThreeVector(0.,0.,-102.7*mm), logicDetHolder3, "logicDetHolder3", logicWorld, false, 0, checkOverlaps);
+  new G4PVPlacement(0, G4ThreeVector(0.,0.,-68*mm), logicDetShieldPb, "logicDetShieldPb", logicWorld, false, 0, checkOverlaps);
 
  // Detector casing
 
- G4VSolid* DetectorCasing1 = new G4Tubs ( "DetectorCasing1", 21/2.*mm, 24/2.*mm, 121.1/2.*mm, 0.*deg, 360.*deg );
+ G4VSolid* DetectorCasing1 = new G4Tubs ( "DetectorCasing1", 61.5/2.*mm, 63.5/2.*mm, 199.5/2.*mm, 0.*deg, 360.*deg );
 
  G4LogicalVolume* logicDetCase1  = new G4LogicalVolume (DetectorCasing1, al_mat, "logicDetCase1");
 
- new G4PVPlacement(0, G4ThreeVector(0.,0.,-84.25*mm), logicDetCase1, "logicDetCase1", logicWorld, false, 0, checkOverlaps);
+ new G4PVPlacement(0, G4ThreeVector(0.,0.,-36.25*mm), logicDetCase1, "logicDetCase1", logicWorld, false, 0, checkOverlaps);
 
- G4VSolid* DetectorCasing2 = new G4Tubs ( "DetectorCasing2", 0*mm, 24/2.*mm, 1.2/2.*mm, 0.*deg, 360.*deg );
+ G4VSolid* DetectorCasing2 = new G4Tubs ( "DetectorCasing2", 0*mm, 63.5/2.*mm, 0.5/2.*mm, 0.*deg, 360.*deg );
 
  G4LogicalVolume* logicDetCase2  = new G4LogicalVolume (DetectorCasing2, al_mat, "logicDetCase2");
 
- new G4PVPlacement(0, G4ThreeVector(0.,0.,-23.1*mm), logicDetCase2, "logicDetCase2", logicWorld, false, 0, checkOverlaps);
-
- G4VSolid* DetectorCasing3 = new G4Tubs ( "DetectorCasing3", 0*mm, 21/2.*mm, 4.1/2.*mm, 0.*deg, 360.*deg );
-
- G4LogicalVolume* logicDetCase3  = new G4LogicalVolume (DetectorCasing3, al_mat, "logicDetCase3");
-
- new G4PVPlacement(0, G4ThreeVector(0.,0.,-35.45*mm), logicDetCase3, "logicDetCase3", logicWorld, false, 0, checkOverlaps);
-
- G4VSolid* DetectorCasing4 = new G4Tubs ( "DetectorCasing4", 0*mm, 2.3/2.*mm, 106.2/2.*mm, 0.*deg, 360.*deg );
-
- G4LogicalVolume* logicDetCase4 = new G4LogicalVolume (DetectorCasing4, al_mat, "logicDetCase4");
-
- new G4PVPlacement(0, G4ThreeVector(0.,0.,-90.6*mm), logicDetCase4, "logicDetCase4", logicWorld, false, 0, checkOverlaps);
-
- G4VSolid* DetectorCasing5 = new G4Tubs ( "DetectorCasing5", 0*mm, 21/2.*mm, 0.8/2.*mm, 0.*deg, 360.*deg );
-
- G4LogicalVolume* logicDetCase5 = new G4LogicalVolume (DetectorCasing5, pcb_mat, "logicDetCase5");
-
- new G4PVPlacement(0, G4ThreeVector(0.,0.,-33*mm), logicDetCase5, "logicDetCase5", logicWorld, false, 0, checkOverlaps);
-
- //Detector volume
-
- G4VSolid* Detector = new G4Box ( "Detector", 10/2.*mm, 10/2.*mm, 5/2.*mm );
+ new G4PVPlacement(0, G4ThreeVector(0.,0.,63.75*mm), logicDetCase2, "logicDetCase2", logicWorld, false, 0, checkOverlaps);
 
 
  //Active detector volume
 
- G4VSolid* DetectorActive = new G4Box ( "DetectorActive", 8.875/2.*mm, 8.875/2.*mm, 3.25/2.*mm );
+ G4VSolid* DetectorActive = new G4Tubs ( "DetectorActive", 0.*mm, 50.8/2.*mm, 12.7/2.*mm, 0.*deg, 360.*deg );
 
- G4LogicalVolume* logicDetector = new G4LogicalVolume (DetectorActive, czt_mat, "logicDetector");
+ G4LogicalVolume* logicDetector = new G4LogicalVolume (DetectorActive, LaBr_mat, "logicDetector");
 
- new G4PVPlacement(0, G4ThreeVector(0.,0.,-29.35*mm), logicDetector, "logicDetector", logicWorld, false, 0, checkOverlaps);
+ new G4PVPlacement(0, G4ThreeVector(0.,0.,55.15*mm), logicDetector, "logicDetector", logicWorld, false, 0, checkOverlaps);
 
- G4VSolid* DeadLayer = new G4SubtractionSolid ("DeadLayer", Detector, DetectorActive, 0, G4ThreeVector(0.,0.,0.75*mm));
+ G4VSolid* Glass = new G4Tubs ( "Glass", 0.*mm, 50.8/2.*mm, 10/2.*mm, 0.*deg, 360.*deg );
 
- G4LogicalVolume* logicDead = new G4LogicalVolume (DeadLayer, czt_mat, "logicDead");
+ G4LogicalVolume* logicGlass = new G4LogicalVolume (Glass, glass_mat, "logicGlass");
 
- new G4PVPlacement(0, G4ThreeVector(0.,0.,-30.1*mm), logicDead, "logicDead", logicWorld, false, 0, checkOverlaps);
+ new G4PVPlacement(0, G4ThreeVector(0.,0.,-43.8*mm), logicGlass, "logicGlass", logicWorld, false, 0, checkOverlaps);
 
- G4VSolid* SourceCasing1 = new G4Tubs ( "SourceCasing1", 0.*mm, 80/2.*mm, 89/2.*mm, 0.*deg, 360.*deg );
+ /*G4VSolid* SourceCasing1 = new G4Tubs ( "SourceCasing1", 0.*mm, 80/2.*mm, 89/2.*mm, 0.*deg, 360.*deg );
 
  G4VSolid* Source = new G4Tubs ( "Source", 0.*mm, 70/2.*mm, 20.8/2.*mm, 0.*deg, 360.*deg );
 
@@ -270,7 +228,7 @@ G4VPhysicalVolume* DetectorConstruction::Construct()
 
  G4LogicalVolume* logicSource  = new G4LogicalVolume (Source, uo_mat, "Source");
 
- new G4PVPlacement(0, G4ThreeVector(0.,0.,11.4*mm), logicSource, "logicSource", logicWorld, false, 0, checkOverlaps);
+ new G4PVPlacement(0, G4ThreeVector(0.,0.,11.4*mm), logicSource, "logicSource", logicWorld, false, 0, checkOverlaps);*/
 
  fScoringVolume = logicDetector;
                                  
